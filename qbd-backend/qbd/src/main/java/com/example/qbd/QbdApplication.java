fae3437b2313eee7398307285fac7c5b2477aae1
Kai <ericson.kai.o@gmail.com>
2025-12-02 14:58:10 +0100

    Set up spring boot

---
+++ qbd-backend/qbd/src/main/java/com/example/qbd/QbdApplication.java
@@ -0,0 +1,13 @@
+package com.example.qbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QbdApplication.class, args);
+	}
+
+}
---
+++ qbd-backend/qbd/src/test/java/com/example/qbd/QbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
