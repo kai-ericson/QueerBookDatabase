ad95885f28e733de4016b7f074d9e9afeb1816f8
Kai <ericson.kai.o@gmail.com>
2026-01-08 10:41:30 +0100

    exclude data source configuration for now

--- qbd-backend/qbd/src/main/java/com/example/qbd/QbdApplication.java
+++ qbd-backend/qbd/src/main/java/com/example/qbd/QbdApplication.java
@@ -2,12 +2,12 @@ package com.example.qbd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class QbdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QbdApplication.class, args);
 	}
-
 }
