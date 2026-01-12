82907db98731ee53a2a76cb00b33c005252cae4e
Kai <ericson.kai.o@gmail.com>
2026-01-12 13:58:35 +0100

    Create controller class for fetching book data

---
+++ qbd-backend/qbd/src/main/java/com/example/qbd/controllers/BookController.java
@@ -0,0 +1,10 @@
+package com.example.qbd.controllers;
+
+public class BookController {
+
+    /**
+     * TODO: Connect to database
+     * TODO: Fetch book data
+     * TODO: Make into BookDTO format
+     */
+}
