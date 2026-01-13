700f58b55f400ec0107a8cfbf0d171b66a8dc9df
Kai <ericson.kai.o@gmail.com>
2026-01-13 13:42:16 +0100

    Update BookDTO record

--- qbd-backend/qbd/src/main/java/com/example/qbd/BookDTO.java
+++ qbd-backend/qbd/src/main/java/com/example/qbd/BookDTO.java
@@ -2,5 +2,5 @@ package com.example.qbd;
 
 import java.time.LocalDate;
 
-public record BookDTO(String title, String author, String description, TagType[] tags, LocalDate publicationDate, String language) {
+public record BookDTO(String title, String author, String description, Tag[] tags, LocalDate publicationDate, String language) {
 }
