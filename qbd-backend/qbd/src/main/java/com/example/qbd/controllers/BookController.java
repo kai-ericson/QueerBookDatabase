11cd5db5fa1ba839d12fc090a27891932350b9c8
Kai <ericson.kai.o@gmail.com>
2026-01-13 13:39:51 +0100

    Fetch all books

--- qbd-backend/qbd/src/main/java/com/example/qbd/controllers/BookController.java
+++ qbd-backend/qbd/src/main/java/com/example/qbd/controllers/BookController.java
@@ -1,10 +1,28 @@
 package com.example.qbd.controllers;
 
-public class BookController {
+import com.example.qbd.enteties.Book;
+import com.example.qbd.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-    /**
-     * TODO: Connect to database
-     * TODO: Fetch book data
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class BookController {
+    @Autowired
+    public final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+    @GetMapping("/books")
+    public List<Book> getAllBooks() {
+        return bookService.getBooks();
+    }
+    /*
      * TODO: Make into BookDTO format
      */
 }
