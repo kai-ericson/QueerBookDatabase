2d7b3b5fa0707a3178d97036a4eefcad3737e853
Kai <ericson.kai.o@gmail.com>
2026-01-13 13:40:57 +0100

    Add BookRepository interface

---
+++ qbd-backend/qbd/src/main/java/com/example/qbd/repositories/BookRepository.java
@@ -0,0 +1,10 @@
+package com.example.qbd.repositories;
+
+import com.example.qbd.enteties.Book;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface BookRepository extends MongoRepository<Book, String> {
+
+}
