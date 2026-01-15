20c71ff067a3caaf5130103aee48d8c5651ceef8
Kai <ericson.kai.o@gmail.com>
2026-01-15 15:55:19 +0100

    update imports

--- qbd-frontend/src/app/components/component.header.ts
+++ qbd-frontend/src/app/components/component.header.ts
@@ -4,7 +4,6 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './component.header.html',
   styleUrl: './component.header.css'
 })
