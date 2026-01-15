55d3ab8a1be20965943b637cbdec7dec79ecb9da
Kai <ericson.kai.o@gmail.com>
2026-01-15 15:56:19 +0100

    update routes and providers

--- qbd-frontend/src/app/app.routes.ts
+++ qbd-frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { app_startpage } from './components/component.homepage'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',redirectTo: 'books', pathMatch: 'full'},
+    {path: 'books', component: app_startpage}
+];
