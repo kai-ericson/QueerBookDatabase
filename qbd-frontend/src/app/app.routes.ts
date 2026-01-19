333ef78bf2eeff68faca5166a1f356c8d1f48ed5
Kai <ericson.kai.o@gmail.com>
2026-01-19 15:52:08 +0100

    add route

--- qbd-frontend/src/app/app.routes.ts
+++ qbd-frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
-import { app_startpage } from './components/component.homepage'
+import { app_startpage } from './components/component.homepage';
+import { book_details } from './components/component.book_details';
 
 export const routes: Routes = [
     {path: '',redirectTo: 'books', pathMatch: 'full'},
-    {path: 'books', component: app_startpage}
+    {path: 'books', component: app_startpage},
+    {path: 'books/:id', component: book_details}
 ];
