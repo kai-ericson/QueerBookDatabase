f21ef03572ce3677b934988e023416f6ecb7a559
Kai <ericson.kai.o@gmail.com>
2026-01-21 10:35:45 +0100

    Update repository

--- qbd-backend/qbd/src/main/java/com/example/qbd/repositories/BookRepository.java
+++ qbd-backend/qbd/src/main/java/com/example/qbd/repositories/BookRepository.java
@@ -2,7 +2,13 @@ package com.example.qbd.repositories;
 
 import com.example.qbd.enteties.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface BookRepository extends MongoRepository<Book, String> {
+
+    @Query("{ 'tags.title':  { $all:  ?0 } }")
+    List<Book> findByTags(List<String> tagTitles);
 
 }
