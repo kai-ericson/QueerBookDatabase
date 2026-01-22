bda3c84aae47f46ab4b2a9d9304de66c46067950
Kai <ericson.kai.o@gmail.com>
2026-01-22 18:43:37 +0100

    update list component

--- qbd-frontend/src/app/components/component.book_list.ts
+++ qbd-frontend/src/app/components/component.book_list.ts
@@ -1,54 +1,47 @@
-import { Component } from '@angular/core';
-import { Book } from '../models/book';
-import { BookService } from '../services/book_service';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { book_details } from './component.book_details';
+import { Book } from '../models/book';
+import { tag } from '../tags/tag';
 
 @Component({
-  selector: 'app-book-list',
-  standalone: true,
-  imports: [CommonModule, book_details],
-  templateUrl: './component.book_list.html',
-  styleUrl: './component.book_list.css'
+    selector: 'app-book-list',
+    standalone: true,
+    imports: [CommonModule, book_details],
+    templateUrl: './component.book_list.html',
+    styleUrl: './component.book_list.css'
 })
-export class app_book_list{ 
+export class app_book_list{
     title = 'book-list';
-    books: Book[] = [];
-    currentPage = 0;
-    pageSize = 10;
-    totalPages= 0;
+   // @Input({ required: true }) selectedBookId!: string | null;
+    @Input({ required: true }) books: Book[] = [];
+   // @Input({ required: true }) page: number = 0;
+    @Input({ required: true }) totalPages: number = 0;
+    @Input({ required: true }) currentPage: number = 0;
+
     selectedBookId: string | null = null;
     
-    constructor(private bookService: BookService) {
-        this.loadBooks();
-    }
-    loadBooks(){
-        this.bookService.getBooks(this.currentPage, this.pageSize)
-        .subscribe(page => {
-            this.books = page.content;
-            this.totalPages = page.totalPages;
-            this.currentPage = page.number;
-        })
-    }
-    nextPage(){
-        if(this.currentPage < this.totalPages - 1){
-            this.currentPage++;
-            this.loadBooks();
-        }
-    }
-    prevPage(){
-        if(this.currentPage > 0){
-            this.currentPage --;
-            this.loadBooks();
-        }
-    }
-
+    @Output() next = new EventEmitter<void>();
+    @Output() prev = new EventEmitter<void>();
+    @Output() openBook = new EventEmitter<string>();
+    @Output() closeBook = new EventEmitter<void>();
+    
+  
     openModal(id: string){
-        console.log("Read more clicked, id: "+ id);
         this.selectedBookId = id;
+        this.openBook.emit(id);
     }
 
     closeModal() {
         this.selectedBookId = null;
+        this.closeBook.emit();
+    }
+
+    nextPage(){
+        this.next.emit();
+    }
+
+    prevPage(){
+        this.prev.emit();
     }
 }
