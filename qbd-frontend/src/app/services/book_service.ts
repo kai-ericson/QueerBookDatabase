39e912b67457f21f52a8d9dedaab33b99d740148
Kai <ericson.kai.o@gmail.com>
2026-01-22 15:11:53 +0100

    Add checkboxes and base styling

--- qbd-frontend/src/app/services/book_service.ts
+++ qbd-frontend/src/app/services/book_service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Book } from '../models/book';
 import { Page } from '../page';
+import { tag } from '../tags/tag';
 
 @Injectable({
     providedIn: 'root'
@@ -14,8 +15,19 @@ export class BookService {
 
     constructor(private http:HttpClient) {}
 
-    getBooks(page: number, size: number){
-            return this.http.get<Page<Book>>(`${this.apiUrl}?page=${page}&size=${size}`);
+    getBooks(
+        page: number, 
+        size: number, 
+        tags: tag[]
+    ): Observable<Page<Book>>{
+        let params = new HttpParams()
+        .set('page', page)
+        .set('size', size);
+
+        tags.forEach(tag => {
+            params = params.append('tags', `${tag.type}:${tag.title}`);
+        });
+        return this.http.get<Page<Book>>(this.apiUrl, { params });
     }
 
     getBookById(id: String){
