a98ac3fb2a441fc759ddcbad2063118be476ec23
Kai <ericson.kai.o@gmail.com>
2026-01-22 18:45:34 +0100

    update services

--- qbd-frontend/src/app/services/book_service.ts
+++ qbd-frontend/src/app/services/book_service.ts
@@ -15,20 +15,27 @@ export class BookService {
 
     constructor(private http:HttpClient) {}
 
-    getBooks(
+    /*getBooks(
         page: number, 
         size: number, 
-        tags: tag[]
+        tagTitles: string[]
     ): Observable<Page<Book>>{
         let params = new HttpParams()
         .set('page', page)
         .set('size', size);
 
-        tags.forEach(tag => {
-            params = params.append('tags', `${tag.type}:${tag.title}`);
+        tagTitles.forEach(tagTitle => {
+            params = params.append('tags', tagTitle);
         });
         return this.http.get<Page<Book>>(this.apiUrl, { params });
-    }
+    }*/
+   getBooks(page: number, size: number, tags: string[]):Observable<{ content: Book[], totalPages: number}>{
+        let params = new HttpParams()
+        .set('page', page.toString())
+        .set('size', size.toString());
+        tags.forEach(tag => params = params.append('tags', tag));
+        return this.http.get<{ content:Book[], totalPages: number}>(this.apiUrl, { params });
+   }
 
     getBookById(id: String){
         return this.http.get<Book>(`${this.apiUrl}/${id}`);
